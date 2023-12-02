8275f9cef019703c15a9f0bc18d8880a2b3e5e41
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-12-02 22:08:22 +0900

    feat: 첫 페이지 슬라이드 바 생성

--- src/pages/input/index.tsx
+++ src/pages/input/index.tsx
@@ -2,18 +2,36 @@
 import { useRouter } from 'next/router'
 import Layout from '@/components/layout'
 import { motion, useMotionValue, useTransform } from 'framer-motion'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { AppRoutes } from '@/common/Constants'
 
 export default function TailwindExample() {
   const router = useRouter()
+  const [currentWidth, setCurrentWidth] = useState(0) //to set max width depending on current width size
+  const [routerPushed, setRouterPushed] = useState(false)
+
   const x = useMotionValue(0)
-  const newX = useTransform(x, [0, 300], [0.1, 1])
+  const myComponentRef = useRef<HTMLDivElement>(null)
+
+  const newX = useTransform(x, [0, currentWidth - 100], [0, 1])
   const constraintsRef = useRef(null)
 
   useEffect(() => {
-    // x.onChange(() => console.log(x.get()))
-    newX.onChange(() => console.log(newX.get()))
+    newX.onChange(() => {
+      if (newX.get() === 1 && !routerPushed) {
+        setRouterPushed(true)
+        router.push(AppRoutes.inputImage)
+      }
+    })
   }, [x])
+
+  useEffect(() => {
+    if (myComponentRef.current) {
+      const componentWidth = myComponentRef.current.offsetWidth
+      setCurrentWidth(componentWidth)
+    }
+  }, [])
+
   return (
     <Layout>
       <div className="overflow-hidden flex flex-col justify-between items-center rounded-[20px] bg-white w-full max-w-[512px] h-full pt-9 relative">
@@ -32,28 +50,30 @@ export default function TailwindExample() {
             <img src="/images/SocialIcons.png" className="scale-[8]  mb-[120px] " />
           </div>
         </div>
-        <div className="absolute flex flex-col  justify-between bg-white w-full h-1/2 bottom-0 p-12 pt-28">
-          <div className="h-2/3">
-            <div className="font-poppins text-4xl  text-center font-bold ">Let me just be efficient</div>
+        <div className="absolute flex flex-col  justify-between bg-white w-full h-1/2 bottom-0 p-8 pt-20">
+          <div className="h-2/3 flex flex-col items-center">
+            <div className="font-poppins text-4xl  text-center font-bold w-5/6 ">Let me just be efficient</div>
             <div className="text-center mt-3 text-gray-400  ">
               Generate SNS contents and boom!
               <br /> Upload done!
             </div>
           </div>
-          <motion.div className="relative flex justify-center rounded-full items-center bg-orange-pink h-[80px] bottom-0 " ref={constraintsRef}>
-            <div className="text-white">Start Experience</div>
-            <motion.div
-              drag="x"
-              style={{ x }}
-              className="absolute  flex justify-center items-center h-[75px] w-[75px] p-4 rounded-full bg-white left-1"
-              dragConstraints={constraintsRef}
-              dragSnapToOrigin
-            >
-              <span className="text-gray-400 text-xl">{'>'}</span>
-              <span className="text-gray-600 text-xl">{'>'}</span>
-              <span className="text-gray-800 text-xl">{'>'}</span>
+          <div ref={myComponentRef}>
+            <motion.div className="relative flex justify-center rounded-full items-center bg-orange-pink h-[80px] bottom-0 " ref={constraintsRef}>
+              <div className="text-white">Start Experience</div>
+              <motion.div
+                drag="x"
+                style={{ x }}
+                className="absolute  flex justify-center items-center h-[70px] w-[70px]  rounded-full bg-white left-1 cursor-pointer"
+                dragConstraints={constraintsRef}
+                dragSnapToOrigin
+              >
+                <span className="text-gray-400 text-xl">{'>'}</span>
+                <span className="text-gray-600 text-xl">{'>'}</span>
+                <span className="text-gray-800 text-xl">{'>'}</span>
+              </motion.div>
             </motion.div>
-          </motion.div>
+          </div>
         </div>
       </div>
     </Layout>
