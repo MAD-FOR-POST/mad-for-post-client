f3a1d689d20e35229d53441c1a1dd9dc943454c3
gangyeolkim <kkyy3402@naver.com>
2023-12-02 23:05:45 +0900

    fix: Poppins 폰트 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,21 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <main className={poppins.className}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </main>
   )
 }
