d68a216480c4d8bb436ba5c1100c8ab9eeb1196d
gangyeolkim <kkyy3402@naver.com>
2023-12-11 20:40:30 +0900

    fix: - Layout 컴포넌트 모바일 레이아웃 최적화

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 
 interface LayoutProps {
   children: React.ReactNode
@@ -6,9 +6,39 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children, backgroundColor }) => {
+  const [layoutHeight, setLayoutHeight] = useState<number | string>('100vh') // 기본값으로 브라우저 높이를 사용
+
+  const updateLayoutHeight = () => {
+    const isMdMedia = window.matchMedia('(min-width: 768px)').matches
+    if (isMdMedia) {
+      const windowHeight = Math.min(window.innerHeight, 926)
+      setLayoutHeight(windowHeight + 'px')
+    } else {
+      setLayoutHeight('100vh')
+    }
+  }
+
+  const init = () => {
+    updateLayoutHeight()
+    window.addEventListener('resize', updateLayoutHeight)
+  }
+
+  const deInit = () => {
+    window.removeEventListener('resize', updateLayoutHeight)
+  }
+
+  useEffect(() => {
+    init()
+    return () => {
+      deInit()
+    }
+  }, [])
+
   return (
     <div className={'flex items-center justify-center fixed left-0 top-0 right-0 bottom-0'} style={{ backgroundColor: backgroundColor ?? '#E2D9E2' }}>
-      <div className={'flex flex-col justify-start box-border items-center bg-white w-[428px] h-full md:h-[926px] overflow-y-scroll hide-scrollbar'}>{children}</div>
+      <div style={{ height: layoutHeight }} className={'flex flex-col justify-start box-border items-center bg-white w-[428px] h-full overflow-y-scroll hide-scrollbar'}>
+        {children}
+      </div>
     </div>
   )
 }
